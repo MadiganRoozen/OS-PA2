79a95d7ff68a5be60304a650971d3bf05195cdcc
Madigan Roozen <98923574+MadiganRoozen@users.noreply.github.com>
2025-03-26 12:24:34 -0400

    Create hash_table.c

---
+++ hash_table.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <thread_manager.h>
+#include <hash_table.h>
+#include <linked_list.h>
+#include <rw_lock.h>
+#include <output.h>
+
+int hash_insert(int key, hashRecord value){
+  
+}
+
+int hash_delete(int key){
+  
+}
+
+hashRecord hash_search(int key){
+  
+}
