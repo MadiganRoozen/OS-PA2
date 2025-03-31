706d37b9ecf8fb3d05993ca96c01fa5a148d52ec
joeydgat <joeydgat@gmail.com>
2025-03-31 19:53:02 -0400

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -1,1 +1,10 @@
 
+#include <stdint.h>
+#include <stdio.h>
+#include <hash_table.h> 
+
+hashRecord* list_insert(hashRecord* head, uint32_t hash, const char* name, uint32_t salary);
+hashRecord* list_delete(hashRecord* head, uint32_t hash);
+hashRecord* list_search(hashRecord* head, uint32_t hash);
+void list_print(FILE* file, hashRecord* head);
+
