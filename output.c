1f835716f6832ffe2d92b49d62d3dd52f9208845
ZoeSchlesinger <zoe.schlesinger@icloud.com>
2025-04-05 16:16:04 -0400

    Added a print just table function + fixed time

--- output.c
+++ output.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <time.h>
+#include "sys/time.h"
 #include <string.h>
-
+#include <pthread.h>
 #include "thread_manager.h"
 #include "hash_table.h"
 #include "linked_list.h"
@@ -11,6 +12,7 @@
 FILE *output;
 int lock_acquisitions = 0;
 int lock_releases = 0;
+static pthread_mutex_t output_lock = PTHREAD_MUTEX_INITIALIZER;
 
 // prepare output file to write in 
 void output_init(){
@@ -22,27 +24,30 @@ void output_init(){
     }
 }
 
-// getting timestamp in milliseconds
-long get_time(){
-    struct timespec time;
-    clock_gettime(CLOCK_REALTIME, &time);
-    return time.tv_sec * 1000 + time.tv_nsec / 1000000;
-}
+// getting timestamp in microseconds
+long long get_time() {
+    struct timeval te;
+    gettimeofday(&te, NULL); // get current time
+    long long microseconds = (te.tv_sec * 1000000) + te.tv_usec; // calculate milliseconds
+    return microseconds;
+  }
 
-// writes out command as executed 
+// writes out command as executed -- thread manager 
 void output_write_command(char* cmd, char* name, int salary){
-
+    pthread_mutex_lock(&output_lock);
     int time = get_time();
-    fprintf(output, "%d,%s,%s,%d\n", time, cmd, name, salary);
+    fprintf(output, "%ld,%s,%s,%d\n", time, cmd, name, salary);
     fflush(output);
-
+    pthread_mutex_unlock(&output_lock);
 }
 
-// write out when locks are acquired + released
+// write out when locks are acquired + released -- rw_lock
 void output_lock_status(char* status){
+    pthread_mutex_lock(&output_lock);
     int time = get_time();
-    fprintf(output, "%d,%s\n", time, status);
+    fprintf(output, "%ld,%s\n", time, status);
     fflush(output);
+    pthread_mutex_unlock(&output_lock);
 
     // count the lock statuses for later 
     if (strstr(status, "ACQUIRED")){
@@ -53,26 +58,37 @@ void output_lock_status(char* status){
     }
 }
 
-// write out when an operation is waiting on a CV  + when it's signaling
+// write out when an operation is waiting on a CV  + when it's signaling -- 
 void output_condition_variables(char* signal){
+    pthread_mutex_lock(&output_lock);
     int time = get_time();
-    fprintf(output, "%d,%s\n", time, signal);
+    fprintf(output, "%ld,%s\n", time, signal);
     fflush(output);
+    pthread_mutex_unlock(&output_lock);
+}
+
+// prints just table 
+void print_table(){
+    pthread_mutex_lock(&output_lock);
+
+    // list of records sorted by hash values 
+    node* curr = list.head; 
+    while (curr) {
+        fprintf(output, "%d,%s,%d", curr->records->hash, curr->records->name, curr->records->salary);
+        curr = curr->next;
+    } 
+
+    fclose(output);
+    pthread_mutex_unlock(&output_lock);
 }
 
 // final print
 void output_final(){
-
     fprintf(output, "\nNumber of lock acquisitions: %d\n", lock_acquisitions);
     fprintf(output, "Number of lock releases: %d\n", lock_releases);
 
     // list of records sorted by hash values 
-    hashRecord* records = sorted_table(); 
-    while (records) {
-        fprintf(output, "%ld,%s,%d", records->hash, records->name, records->salary);
-        records = records->next;
-    } 
+    print_table();
 
     fclose(output);
-
 }
--- output.h
+++ output.h
@@ -1,6 +1,7 @@
 void output_init();
-long get_time();
+long long get_time();
 void output_write_command(char* cmd, char* name, int salary);
 void output_lock_status(char* status);
 void output_condition_variables(char* signal);
+void print_table();
 void output_final();
