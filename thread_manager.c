bc8394348b922de600eb657d9ade35ff28a1a882
ebakire1 <evansbakire@outlook.com>
2025-04-06 19:00:46 -0400

    Added the execute_commands() function

--- thread_manager.c
+++ thread_manager.c
@@ -23,10 +23,43 @@ void init_thread_manager(int total) {
     command_count = 0;
 }
 
-// Help Perform the required hash table operations ? I didnt see it anywhere in our files
-extern void execute_commands(char *command){
+
+
+// New function: execute_commands() now parses the command string and calls the corresponding hash table function.
+void execute_commands(char *cmd) {
+    char command[20], name[50];
+    int salary;
     
+    // expected format "command,name,salary"
+    sscanf(cmd, "%[^,],%[^,],%d", command, name, &salary);
+    
+    if (strcmp(command, "insert") == 0) {
+        //  hash table insert function 
+        hash_insert(name, (uint32_t)salary);
+        output_write_command("INSERT", name, salary);
+    } 
+    else if (strcmp(command, "delete") == 0) {
+        // hash value for the key and call delete.
+        uint32_t key_hash = jenkins_one_at_a_time_hash((uint8_t*)name, strlen(name));
+        hash_delete(key_hash);
+        output_write_command("DELETE", name, salary);
+    } 
+    else if (strcmp(command, "search") == 0) {
+        //  hash value for the key and call search.
+        uint32_t key_hash = jenkins_one_at_a_time_hash((uint8_t*)name, strlen(name));
+        hashRecord *result = hash_search(key_hash);
+        output_write_command("SEARCH", name, salary);
+       
+    } 
+    else if (strcmp(command, "print") == 0) {
+        // Print the entire table.
+        print_table();
+        output_write_command("PRINT", "", 0);
+    }
 }
+
+
+
 
 // Enqueue a command string into the global command array.
 void enqueue_command(char *command) {
